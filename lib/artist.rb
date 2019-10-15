b0858c4caa93962a6d10237737d238b88b07cf93
James Batsell <jamesbatsell05@gmail.com>
2019-10-15 20:54:58 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+class Artist
+  attr_accessor :name
+  attr_reader :songs
+  
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
+  
+
+  @@artists = []
+
+  def initialize
+    super
+    @songs = []
+  end
+
+  def self.all
+    @@artists
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def add_songs(songs)
+    songs.each { |song| add_song(song) }
+  end
+end
